66719be96c1f296a681f8fab9aa2c644f0c225d5
Jonathan Valencia <javalencis@gmail.com>
2022-09-24 17:21:12 -0500

    Diseño de tabla

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,26 +1,23 @@
 import { FilterSearch } from "../components/FilterSearch"
 import { Header } from "../components/Header"
 import { LateralLeft } from "../components/LateralLeft"
-import { RowUser } from "../components/RowUser"
 import { UsersList } from "../components/UsersList"
 import { useFecthUsers } from "../hooks/useFecthUsers"
 
 export const App = () => {
 
 
-  const { data: users, loading:loadingUsers } = useFecthUsers('GET_users')
+  const { data: users, loading: loadingUsers } = useFecthUsers('GET_users')
 
   return (
     <div className="AppContainer">
       <LateralLeft />
       <Header />
       <FilterSearch />
- 
-      {
 
-        !( loadingUsers) && (
+      {
+        !(loadingUsers) && (
           <UsersList users={users} />
-    
         )
       }
 
